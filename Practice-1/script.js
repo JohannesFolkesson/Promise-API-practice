a067ef15084f9a6305be592ceddc0c2a469a483c
Johannes Folkesson <johannes.folkesson@chasacademy.se>
2025-11-04 11:20:30 +0100

    .then

--- Practice-1/script.js
+++ Practice-1/script.js
@@ -1,1 +1,18 @@
-const btn = createElement
+const btn = document.createElement('button');
+btn.textContent = 'Klicka här'
+document.body.appendChild(btn);
+
+btn.addEventListener('click', () => {
+    fetch('https://reqres.in/api/users')
+    .then (res => {
+        if(!res.ok) {
+            console.log('Problem')
+            return
+        }
+
+        return res.json();
+    })
+    .then(data => {
+
+    })
+})
