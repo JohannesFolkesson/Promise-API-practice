636e2cb2a70ed900805ba3fe852c9e760ad08673
Johannes Folkesson <johannes.folkesson@chasacademy.se>
2025-11-04 11:29:37 +0100

    Practice

--- Practice-1/script.js
+++ Practice-1/script.js
@@ -1,18 +1,22 @@
 const btn = document.createElement('button');
-btn.textContent = 'Klicka här'
+btn.textContent = 'Klicka här';
 document.body.appendChild(btn);
 
 btn.addEventListener('click', () => {
-    fetch('https://reqres.in/api/users')
-    .then (res => {
-        if(!res.ok) {
-            console.log('Problem')
-            return
-        }
-
-        return res.json();
-    })
-    .then(data => {
-
-    })
-})
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => {
+            if (!res.ok) throw new Error('Nätverksfel eller ogiltig förfrågan');
+            return res.json();
+        })
+        .then(data => {
+            const user = data[3]; // fjärde användaren i arrayen
+            if (user) {
+                console.log('Användare:', user.name); // använd "name" istället för first_name
+            } else {
+                console.log('Ingen användare på index 3');
+            }
+        })
+        .catch(error => {
+            console.error('Fel:', error.message);
+        });
+});
